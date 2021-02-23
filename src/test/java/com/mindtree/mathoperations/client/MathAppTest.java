7bbed786eb565e129b2e3bf57b9cf2d198b3cc0d
RaviTeja-Manepalli <ravitejamanepalli47@gmail.com>
2021-02-23 17:08:38 +0530

    Second commit

--- src/test/java/com/mindtree/mathoperations/client/MathAppTest.java
+++ src/test/java/com/mindtree/mathoperations/client/MathAppTest.java
@@ -10,18 +10,26 @@ import com.mindtree.mathoperations.service.ServiceImpl;
 
 public class MathAppTest {
 
+	MathApp math = new MathApp();
 	Service ser = new ServiceImpl();
 	int value1 = 4;
 	int value2 = 5;
 
 	@Test
-	public void test() {
-
+	public void addtest() {
+		
 		int result = ser.addition(value1, value2);
 		assertTrue(result == 9);
+
+	}
+	
+	@Test
+	public void subtest() {
+
 		int result2 = ser.subtraction(value1, value2);
 		assertTrue(result2 == -1);
 
 	}
+
 
 }
